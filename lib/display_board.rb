609b86c66e058f9be4f601aa8533491241be45eb
Laura Trager <laura.trager@gmail.com>
2017-09-27 00:40:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 def display_board
   cell = "   "
   row = "#{cell}|#{cell}|#{cell}
------------"
-  print row
-  print row
+-----------
+#{cell}|#{cell}|#{cell}
+-----------
+#{cell}|#{cell}|#{cell}"
 end
